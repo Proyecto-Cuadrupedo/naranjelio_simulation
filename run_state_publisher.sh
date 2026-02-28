1f92148e9b8da056205bd8ad3e0f3ab6e10a25e8
dcuevasa <d.cuevas@uniandes.edu.co>
2026-02-28 18:20:58 -0500

    can move joints

--- run_state_publisher.sh
+++ run_state_publisher.sh
@@ -1,1 +1,2 @@
-ros2 run robot_state_publisher robot_state_publisher --ros-args -p robot_description:="$(xacro naranjelio.urdf)"
+export NARANJELIO_HOME=$PWD
+ros2 run robot_state_publisher robot_state_publisher --ros-args -p robot_description:="$(xacro naranjelio.urdf.xacro)" -p use_sim_time:=true
